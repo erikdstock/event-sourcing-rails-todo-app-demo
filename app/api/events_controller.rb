3378a1a391b7e89c660faec57fda964d728263be
Erik Stockmeier <erik.d.stock@gmail.com>
2021-04-06 12:18:15 -0400

    set up grape api

---
+++ app/api/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < Grape::API
+  desc 'create an event directly '
+  params do
+    requires :type, type: String, desc: 'A deserializeable event class (Minus `Events::`)'
+    requires :body, type: Hash, desc: 'keyword args for the event constructor'
+  end
+
+  post :events do
+    event_type = params[:type]
+    error!({ message: 'event.type must inherit from Lib::BaseEvent' }, 401) unless event_type
+    klass = ('Events::' + event_type).classify&.constantize
+    unless klass&.ancestors&.include? Lib::BaseEvent
+      error!({ message: 'event.type must inherit from Lib::BaseEvent' }, 401)
+    end
+    event = klass.new(params[:body])
+    if event.save
+      event.reload.aggregate
+    else
+      error!({ message: event.errors.full_messages }, 401)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/events', to: 'events#create'
+  # post '/events', to: 'events#create'
+  mount API => '/'
 end
