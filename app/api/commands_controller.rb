95a557ba968ad42b94bfdaf83941b12313dd9efd
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-04-16 12:30:42 -0400

    refactor(api): created nested namespace for list commands

--- app/api/commands_controller.rb
+++ app/api/commands_controller.rb
@@ -1,16 +1,18 @@
 class CommandsController < Grape::API
   namespace :commands do
-    desc 'create a new todo list'
-    params do
-      requires :name, type: String
-      optional :metadata, type: Hash, desc: 'metadata associated with the event', default: {}
-    end
-    post :create_list do
-      command = Commands::TodoList::Create.new(name: params[:name], metadata: params[:metadata])
-      if command.valid?
-        command.call
-      else
-        error!({ errors: command.errors.full_messages }, 400)
+    namespace :list do
+      desc 'create a new todo list'
+      params do
+        requires :name, type: String
+        optional :metadata, type: Hash, desc: 'metadata associated with the event', default: {}
+      end
+      post :create_list do
+        command = Commands::TodoList::Create.new(name: params[:name], metadata: params[:metadata])
+        if command.valid?
+          command.call
+        else
+          error!({ errors: command.errors.full_messages }, 400)
+        end
       end
     end
   end
--- spec/requests/commands_spec.rb
+++ spec/requests/commands_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe 'Commands', type: :request do
     it 'creates a new list' do
       expect(TodoList.all.count).to equal 0
 
-      expect { post '/api/commands/create_list', params: { name: 'Test List' } }
+      expect { post '/api/commands/list/create_list', params: { name: 'Test List' } }
         .to change { TodoList.all.count }.by 1
 
       expect(TodoList.last.name).to eq 'Test List'
     end
 
     it 'does not create a list that already exists' do
-      post '/api/commands/create_list', params: { name: 'Test List' }
+      post '/api/commands/list/create_list', params: { name: 'Test List' }
 
-      expect { post '/api/commands/create_list', params: { name: 'Test List' } }
+      expect { post '/api/commands/list/create_list', params: { name: 'Test List' } }
         .to change { TodoList.all.count }.by 0
 
       expect(JSON.parse(response.body)['errors']).to eq ['List name must be unique']
